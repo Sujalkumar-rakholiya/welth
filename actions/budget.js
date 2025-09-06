9dcd6bcafe4ece63aa6e107cdf0c181bdc96f2f2
Sujalkumar Rakholiya <sujalrakholiya69@gmail.com>
2025-09-06 13:10:24 +0530

    update message

---
+++ actions/budget.js
@@ -0,0 +1,18 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+
+export async function getCurrentBudget(accountId) {
+    try {
+        const { userId } = await auth();
+        if (!userId) throw new Error("Unauthenticated");
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+    } catch (error) { }
+}
