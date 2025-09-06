50e31ffecf0a8ba78443f5dc00ee94d10aa6c476
Sujalkumar Rakholiya <sujalrakholiya69@gmail.com>
2025-09-06 13:15:56 +0530

    update message

--- actions/budget.js
+++ actions/budget.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
 export async function getCurrentBudget(accountId) {
@@ -14,5 +15,23 @@ export async function getCurrentBudget(accountId) {
         if (!user) {
             throw new Error("User not found");
         }
+
+        const budget = await db.budget.findFirst({
+            where: {
+                userId: user.id,
+            },
+        });
+
+        const currentDate = new Date();
+        const startOfMonth = new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth(),
+            1
+        );
+        const endOfMonth = new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth() + 1,
+            0
+        );
     } catch (error) { }
 }
