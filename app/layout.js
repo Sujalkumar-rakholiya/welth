6ab583b8b8b98c942b6f3bdc0fa212582533188d
Sujalkumar Rakholiya <sujalrakholiya69@gmail.com>
2025-09-06 11:29:50 +0530

    update message

--- app/layout.js
+++ app/layout.js
@@ -7,30 +7,30 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Welth",
-  description: "Finance Platform",
+    title: "Welth",
+    description: "Finance Platform",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className}`}>
-          {/* header */}
-          <Header />
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className}`}>
+                    {/* header */}
+                    <Header />
 
-          <main className="min-h-screen">
-            {children}
-          </main>
-          <Toaster richColors />
-          {/* footer */}
-          <footer className="bg-blue-50 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-600">
-             
-            </div>
-          </footer>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+                    <main className="min-h-screen">
+                        {children}
+                    </main>
+                    <Toaster richColors />
+                    {/* footer */}
+                    <footer className="bg-blue-50 py-12">
+                        <div className="container mx-auto px-4 text-center text-gray-600">
+
+                        </div>
+                    </footer>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
